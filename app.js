6088e1d1965a196aed33a20cd4d6b57f0eb8bd55
Mostafa mohamed <thedevelopermostafa@gmail.com>
2023-05-07 21:37:30 +0300

    Updated the countDownToNewYear function, so that it tracks the new year permanently

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 function countDownToNewYear() {
-  const now = new Date();
-  const newYear = new Date(2023, 0, 1);
+  let currentYear = new Date();
+  document.querySelector('#current-year').textContent =
+    currentYear.getFullYear();
 
-  const diff = newYear - now;
+  let newYear = currentYear.getFullYear() + 1;
+  document.querySelector('#new-year').textContent = newYear;
+
+  const diff = new Date(newYear, 0, 1) - currentYear;
 
   const days = Math.floor(diff / 1000 / 3600 / 24);
   const hours = Math.floor((diff / 1000 / 3600) % 24);
   const mins = Math.floor((diff / 1000 / 60) % 60);
   const seconds = Math.floor((diff / 1000) % 60);
-  document.getElementById("days").innerText = days;
-  document.getElementById("hours").innerText = hours;
-  document.getElementById("mins").innerText = mins;
-  document.getElementById("seconds").innerText = seconds;
+
+  document.getElementById('days').innerText = diff > 0 ? days : 0;
+  document.getElementById('hours').innerText = diff > 0 ? hours : 0;
+  document.getElementById('mins').innerText = diff > 0 ? mins : 0;
+  document.getElementById('seconds').innerText = diff > 0 ? seconds : 0;
 }
+
 countDownToNewYear();
+
 setInterval(() => {
   countDownToNewYear();
 }, 1000);
